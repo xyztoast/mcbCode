305a8b83468831ee2b3ba945e11da7d12b3c0790
toast <244019107+xyztoast@users.noreply.github.com>
2026-05-05 16:26:46 -0700

    add login check

--- backendr/ads.js
+++ backendr/ads.js
@@ -1,9 +1,11 @@
-function loadAdsense() {
+function isLoggedIn() {
+    return localStorage.getItem("loggedIn") === "true";
+}
+
+if (!isLoggedIn()) {
     const script = document.createElement("script");
     script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2825557346768110";
     script.async = true;
     script.crossOrigin = "anonymous";
     document.head.appendChild(script);
 }
-
-loadAdsense();
